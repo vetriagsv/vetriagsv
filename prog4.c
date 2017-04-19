21582bcd81553039e1ed92f75d0c4d665c914d0b
vetriagsv <vetriarul2395@gmail.com>
2017-04-19 15:18:46 +0530

    Add files via upload

---
+++ prog4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char n;
+    printf("enter the n");
+    scanf("%c",&ch);
+    if((n>="A"&&n<="Z")||(n>="a"&&n<="z"))
+        printf("n is alphabet");
+    else
+        printf("n is number")
+}
