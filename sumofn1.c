133ca692ff293871d3cbc5f27c02added44facf9
vetriagsv <vetriarul2395@gmail.com>
2017-06-05 11:12:41 +0530

    Add files via upload

---
+++ sumofn1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,s;
+    printf("enter the n value");
+    scanf("%d",&n);
+    s=(n+(n*n))/2;
+    printf("",s);
+    getch();
+
+
+}
