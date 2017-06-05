eed154bf5691f67629dd07518905a8662cc12995
vetriagsv <vetriarul2395@gmail.com>
2017-06-05 14:01:45 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,s;
+    printf("Enter the value of the number");
+    scanf("%d",&i);
+    s=i^i;
+    printf("",s);
+    getch();
+}
