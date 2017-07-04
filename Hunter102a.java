892d867acc3b7473163f4f148db74342c7abb33e
vetriagsv <vetriarul2395@gmail.com>
2017-07-04 10:33:10 +0530

    Add files via upload

---
+++ Hunter102a.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hunter102a {
+	public static void main(String[] args){
+		int sum=0;
+		int answer=0;int answer2=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int number=s.nextInt();
+		String str=String.valueOf(number);
+		for(int i=0;i<=str.length()-1;i++){
+			int num=Integer.parseInt(str.substring(i,i+1));
+			sum=sum+num;
+			answer=answer+sum;
+		}
+		System.out.println(answer);
+	}
+
+}
