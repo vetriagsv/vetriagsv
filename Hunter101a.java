7674b46a2790914885aefa39ea8f980dcb429940
vetriagsv <vetriarul2395@gmail.com>
2017-07-04 09:54:36 +0530

    Add files via upload

---
+++ Hunter101a.java
@@ -0,0 +1,15 @@
+
+public class Hunter101 {
+	public static void main(String[] args){
+		for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+            
+		}
+	}
+}
