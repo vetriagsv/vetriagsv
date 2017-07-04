3e2adaa01037d38f848cae43567ab48a48e92079
vetriagsv <vetriarul2395@gmail.com>
2017-07-04 11:25:04 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number of elements in the array:");
+		int num=s.nextInt();
+		int arr[]=new int[num];
+		int count[] = null;
+		System.out.println("Enter the elements:");
+		for(int i=0;i<num;i++){
+			arr[i]=s.nextInt();
+		}
+		for(int j=0;j<arr.length-1;j++){
+			for(int k=j+1;k<arr.length;k++){
+			if(arr[j]<arr[k]){
+				int temp=arr[k];
+				arr[k]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	 }
+	for(int m=0;m<arr.length;m++){
+		System.out.println(arr[m]);
+    }
+  }
+}
+
